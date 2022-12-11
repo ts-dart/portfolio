1bd2acee70032b31afea288003762cd08b9b85b4
thiago <thiagoedusan5.11@outlook.com>
2022-12-11 01:08:01 -0300

    feat: organizando a pagina

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import '../styles/Header.css';
 import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
--- app/src/components/TextImgContent.tsx
+++ app/src/components/TextImgContent.tsx
@@ -1,6 +1,8 @@
+import '../styles/TextImgContent.css';
+
 export default function TextImgContent() {
     return(
-        <div>
+        <div id='TextImgContent'>
             <h1>Óla U+1F596.<br/>me chamo Thiago Henrique<br/>sou desenvolvedor web full-stack</h1>
             <img src='images/undraw_coding_re_iv62.svg' alt='img'/>
         </div>
